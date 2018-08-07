f1cdc605be0da0a0e0ccbf50fad8911bb06748bf
leen19-meet <leen19@meet.mit.edu>
2018-08-07 10:26:15 +0300

    Day8

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -19,16 +19,43 @@ def add_article(artName, topic, rating):
 
 
 def query_all_articles():
-	pass
+	knowledge = session.query(
+		Knowledge).all()
+	return knowledge 
+	
 
-def query_article_by_topic():
-	pass
+def query_article_by_topic(their_topic):
+	knowledge  = session.query(
+		Knowledge).filter_by(
+			topic = their_topic).all()
+	return knowledge 
+	
+#def query_article_by_rating(threshold):
 
-def delete_article_by_topic():
-	pass
+
+
+def delete_article_by_topic(topic):
+	session.query(Knowledge).filter_by(
+		topic = topic).delete()
+	session.commit() 
+	
 
 def delete_all_articles():
-	pass
+	session.query(Knowledge).delete()
+	session.commit()
+
+def edit_rating(updated_rating, article_title):
+	knowledge = session.query(Knowledge).filter_by(article_name = article_title).first()
+	knowledge.rating  = updated_rating 
+	session.commit()
+
+
+
 
 def edit_article_rating():
 	pass
+
+
+Name  | Year
+Subhi | 2001
+Ahmad | 1991
